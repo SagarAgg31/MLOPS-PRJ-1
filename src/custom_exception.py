a70ce81c2bd048ab28ba9134f5b1682cf83cd824
SAGAR AGGARWAL <aggarwal3110@gmail.com>
2025-04-02 17:29:47 +0530

    Dixed

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -3,17 +3,19 @@ import sys
 
 class CustomException(Exception):
 
-    def __init__(self, error_message, error_detail):
+    def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+        self.error_message = self.get_detailed_error_message(error_message,error_detail)
 
     @staticmethod
-    def get_detailed_error_message(error_message, error_detail):
-        _, _, exc_tb = traceback.sys.exc_info()
-        filename = exc_tb.tb_frame.f_code.co_filename
-        line_number = exc_tb.tb_lineno
-        return f"Error occurred in script: {filename} at line {line_number} - {error_message}"
+    def get_detailed_error_message(error_message , error_detail:sys):
 
+        _, _, exc_tb = traceback.sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        return f"Error in {file_name} , line {line_number} : {error_message}"
+    
     def __str__(self):
         return self.error_message
-    
+
